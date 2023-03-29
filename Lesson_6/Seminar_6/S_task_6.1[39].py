ec2e02b570e80c1b47943dfaa63919ff79558821
Btightvl work <ziro-x-100@mail.ru>
2023-03-29 13:09:20 +0300

    HW_6 Done!

--- Lesson_6/Seminar_6/S_task_6.1[39].py
+++ Lesson_6/Seminar_6/S_task_6.1[39].py
@@ -11,15 +11,18 @@
 # <function_name>([3, 1, 3, 4, 2, 4, 12], [4, 15, 43, 1, 15, 1] ) -> [3, 3, 2, 12]
 # <function_name>([3, 4, 1, 5, 1, 3, 10, 4, 9, 5], [9, 6, 6, 5, 10, 1, 10, 9, 1, 5] ) -> [3, 4, 3, 4]
 
-def funk (list1, list2):
-    return  list(set(list1).difference(set(list2)))
+def funk(list1, list2):
+    return list(set(list1).difference(set(list2)))
+
 
 # print(funk([3, 1, 3, 4, 2, 4, 12], [4, 15, 43, 1, 15, 1]))
 
-def funk2 (list1, list2):
+def funk2(list1, list2):
     new_list = list()
     for i in list1:
         if i not in list2 and i not in new_list:
             new_list.append(i)
     return new_list
+
+
 print(funk2([3, 1, 3, 4, 2, 4, 12], [4, 15, 43, 1, 15, 1]))
