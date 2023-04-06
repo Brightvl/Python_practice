b14317a17b12dbf91265b960d9fdcb54c925c875
Brightvl_pc <ziro-x-100@mail.ru>
2023-04-07 01:26:09 +0300

    Lesson_8 Лекция добита осталась HW

--- Lesson_8/S_or_HW_8.py
+++ Lesson_8/S_or_HW_8.py
@@ -1,16 +1,53 @@
-import csv
-
-phonebook = [{"surname": "Иванов", "name": "Иван", "phone": "79147564729", "description": "Начальник"},
-             {"surname": "Иванько", "name": "Сергей", "phone": "79147564465", "description": "Работник"}]
+import os
 
 
-# Отображение справочника?
+# Путь к файлу
+def file_path() -> str:
+    return input("\nУкажите путь к файлу: ")
 
-# Чтение телефонного справочника по фильтру
-def read_records(phonebook):
+
+# Проверка пути
+def path_check(file_path) -> bool:
+    if os.path.isfile(file_path):
+        return True
+    else:
+        return False
+
+
+# Создание новой записи в справочнике
+def create_record(data_phonebook):
+    new_person = {}
+    new_person["surname"] = input("\nВведите фамилию: ").capitalize()
+    new_person["name"] = input("Введите имя: ").capitalize()
+    new_person["phone"] = input("Введите телефон: ")
+    new_person["description"] = input("Введите описание: ").capitalize()
+    return data_phonebook.append(new_person)
+
+
+# Чтение файла и запись в список, чтобы сохранить его в памяти
+def read_file(file_path: str) -> list:
+    data = []
+    # r тип операции - чтение
+    with open(file_path, "r", encoding='utf-8') as file:
+        for string in file:  # перебор строк в файле + \n
+            new_list = string.strip().split(",")  # убираем перенос строки и делим по запятой
+            data.append({"surname": new_list[0], 'name': new_list[1], 'phone': new_list[2], 'description': new_list[3]})
+    return data
+
+
+# Вывод справочника на экран
+def view_phonebook(data_phonebook):
+    print("\nДанные справочника:")
+    for string in data_phonebook:
+        print(f"Фамилия: {string['surname']}, Имя: {string['name']}, Телефон: {string['phone']}, Описание: "
+              f"{string['description']}")
+
+
+# фильтр в справочнике по фамилии
+def read_records_surname(data_phonebook):
     surname_filter = input("Введите первую часть фамилии для поиска: ").capitalize()
     found_records = []
-    for person in phonebook:
+    for person in data_phonebook:
         # Startswith Помогает определить начинается ли строка с символов указанных в скобках. Возвращает True or False
         # get возвращает значение по введенному ключу
         if person.get("surname").startswith(surname_filter):
@@ -23,16 +60,49 @@ def read_records(phonebook):
         print("Записи не найдены.")
 
 
-# Создание новой записи в справочнике
-def create_record(phonebook):
-    new_person = {}
-    new_person["Name"] = input("Введите фамилию: ").capitalize()
-    new_person["surname"] = input("Введите имя: ").capitalize()
-    new_person["phone"] = input("Введите телефон: ")
-    new_person["description"] = input("Введите описание: ").capitalize()
-    return phonebook.append(new_person)
+# Экспорт в файл
+def export_in_file(export_file, data):
+    with open(export_file, "w", encoding='utf-8') as file:
+        for el in data:  # перебор строк в файле + \n
+            file.write(f"{el['surname']},{el['name']},{el['phone']},{el['description']}\n")
 
 
-# read_records(phonebook)
-create_record(phonebook)
-print(phonebook)
+phonebook = []
+while True:
+    print("\nВозможные действия")
+    print("1. Создать запись")
+    print("2. Вывести имеющиеся данные")
+    print("3. Экспортировать данные в файл")
+    print("4. Импортировать данные из файла")
+    print("5. Выход")
+    user_input = input("\nВведите действие > ")
+
+    if user_input == "1":
+        create_record(phonebook)
+    if user_input == "2":
+        if len(phonebook) == 0:
+            print("\nДанные не найдены, проверьте импортировали ли вы список")
+        else:
+            view_phonebook(phonebook)
+    if user_input == "3":
+        if len(phonebook) != 0:
+            export_file_path = file_path()
+            if path_check(export_file_path):
+                # Чтение файла
+                export_in_file(export_file_path, phonebook)
+            else:
+                temp = input(f"файл не найден\nСоздать eго в указанном пути? {export_file_path} Y/N: ").upper()
+                if temp == "Y":
+                    export_in_file(export_file_path, phonebook)
+        else:
+            print("Вы еще ничего не создали! Проверьте импорт.")
+    if user_input == "4":
+        import_file_path = file_path()
+        if path_check(import_file_path):
+            # Чтение файла
+            phonebook = read_file(import_file_path)
+        else:
+            print("\nПуть указан неправильно!")
+    if user_input == "5":
+        print("_______________\nКонец программы")
+        break
